1a6c8b6e5367d66ad1a58f61af4393b86881ef54
xiejiaxin <904073612@qq.com>
2019-03-12 17:02:31 +0800

    两个视频合并前备份

--- android/app/src/main/java/com/firstapp/MainApplication.java
+++ android/app/src/main/java/com/firstapp/MainApplication.java
@@ -3,6 +3,8 @@ package com.firstapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.github.yamill.orientation.OrientationPackage;
+import com.brentvatne.react.ReactVideoPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -26,6 +28,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new OrientationPackage(),
+            new ReactVideoPackage(),
             new VectorIconsPackage(),
             new LinearGradientPackage(),
             new RNGestureHandlerPackage()
